710f02d3cac89dda519c33500aecb0b002e367aa
Landon Colaresi <landoncol3117@gmail.com>
2020-06-19 14:14:46 +0000

    get-CIAD-not-self

--- main.clj
+++ main.clj
@@ -1,17 +1,17 @@
 (defn ciadad [data] (do 
-  (def cia-dists (
-    map data (
-      fn [x] (getCIAD-not-self x data))))
-  (def median-dist (
-    median cia-dists))
-  (map cia-dists (
-    fn [x] (< x median-dist))))
-
+  (def cia-dists
+    (map data
+      (fn [x] (getCIAD-not-self x data))))
+  (def median-dist
+    (median cia-dists))
+  (map cia-dists
+    (fn [x] (< x median-dist))))
 (defn get-CIAD-not-self [point,data] (do 
-  (def options (
-    map data (
-      fn [x] (ciad x point))))
-  (def mindist (
-    apply min options))
-  (def )
-  ))
+  (def options
+    (map data
+      (fn [x] (ciad-not-self x point))))
+  (def mindist
+    (apply min options))
+  (def index-min
+    (.indexOf options mindist))
+  (get options index-min)))
